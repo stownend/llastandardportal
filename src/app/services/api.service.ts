e52f04c90702f30d4479166fe3a58b5a58afb006
Steve Townend <steve.townend@innervision.co.uk>
2022-05-05 12:01:06 +0100

    Changed api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,17 +9,17 @@ import { throwError, of } from 'rxjs';
 })
 export class ApiService {
 
-  apiRoot:string = "https://localhost:44336";
-  tokenRoot: string = "https://llastandardtest1.auth.eu-west-2.amazoncognito.com";
-  client_id: string = "4ba2r5fkr2gth2ki0b8ou4j38a";
-  client_secret: string = "1hpt93tqgiev5joep126fk61lu7us2sa0fpe2frabm4kkon6ngot";
-  scope:string = "https://localhost:44336/api/customerSignUp";
-
-  // apiRoot:string = "https://dev-a.innervision.co.uk";
-  // tokenRoot: string = "https://llastandardstaging1.auth.eu-west-2.amazoncognito.com";
-  // client_id: string = "2n5gosf055f559ek9u5ncv9ckn";
-  // client_secret: string = "1rk320bv2brd4t7abi6ondmi3l490fg6v62sm2cpeopc2gbovfsd";
+  // apiRoot:string = "https://localhost:44336";
+  // tokenRoot: string = "https://llastandardtest1.auth.eu-west-2.amazoncognito.com";
+  // client_id: string = "4ba2r5fkr2gth2ki0b8ou4j38a";
+  // client_secret: string = "1hpt93tqgiev5joep126fk61lu7us2sa0fpe2frabm4kkon6ngot";
   // scope:string = "https://localhost:44336/api/customerSignUp";
+
+  apiRoot:string = "https://dev-a.innervision.co.uk";
+  tokenRoot: string = "https://llastandardstaging1.auth.eu-west-2.amazoncognito.com";
+  client_id: string = "2n5gosf055f559ek9u5ncv9ckn";
+  client_secret: string = "1rk320bv2brd4t7abi6ondmi3l490fg6v62sm2cpeopc2gbovfsd";
+  scope:string = "https://localhost:44336/api/customerSignUp";
 
   constructor(private http: HttpClient) { }
   
